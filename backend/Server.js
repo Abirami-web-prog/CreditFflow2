930dcae3186d24d0d53ced8313547aa745ce0838
Abirami-web-prog <abisscse@gmail.com>
2025-12-10 23:03:16 +0530

    Refactor Server.js to use CommonJS syntax

--- backend/Server.js
+++ backend/Server.js
@@ -1,10 +1,7 @@
-
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import customerRoutes from "./Routes/customerRoutes.js";
-
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const customerRoutes = require("./Routes/customerRoutes");
 
 const app = express();
 
